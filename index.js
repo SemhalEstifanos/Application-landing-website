f35aadae21baf5e9c24adb970cad71158f5f80b3
Student <aberasemhal@gmail.com>
2025-03-27 08:22:14 +0300

    made some changes

--- index.js
+++
@@ -1,8 +0,0 @@
-// let  a=arrays.arr1 = [3,7,34,90,12]
-// let arr2 = ['true', 'green', 'where','12,56'];
-// let arr3=arr2.pop();
-// console.log(arr3);
-
-let arr24 = ['true', 'green', 'where','12,56'];
-// let arr36=arr24.push("semhal");
-console.log(arr24.join(,));
